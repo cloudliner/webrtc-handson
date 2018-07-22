0b25b6818766a8baf9c72c9d4a9a530bee983c6b
cloudliner <cloudliner.jp@gmail.com>
2018-07-22 15:18:52 +0900

    connect room

--- handson/script.js
+++ handson/script.js
@@ -1,5 +1,6 @@
 $(function(){
   let localStream = null;
+  let viedoRoom = null;
 
   navigator.mediaDevices.getUserMedia({video: true, audio: true})
     .then(stream => {
@@ -9,4 +10,16 @@ $(function(){
       console.error(`mediaDevice.getUserMedia() error: ${error}`);
       return;
     });
+
+  $.getJSON(`../video-token`, function(tokenResponse) {
+    console.log(tokenResponse.token);
+    Twilio.Video.connect(tokenResponse.token, {
+      name: 'videoRoom',
+    })
+    .then(room => {
+      console.log(`Connected to Room ${room.name}`);
+      $('#my-id').text(room.localParticipant.identity);
+      videoRoom = room;
+    });  
+  });
 });
