f6e5e06fb7c2850c8aad6357696d0654cdd1b62f
cloudliner <cloudliner.jp@gmail.com>
2018-07-22 15:04:25 +0900

    localstream

--- handson/script.js
+++ handson/script.js
@@ -1,3 +1,12 @@
 $(function(){
+  let localStream = null;
 
+  navigator.mediaDevices.getUserMedia({video: true, audio: true})
+    .then(stream => {
+      $('#myStream').get(0).srcObject = stream;
+      localStream = stream;
+    }).catch(error => {
+      console.error(`mediaDevice.getUserMedia() error: ${error}`);
+      return;
+    });
 });
